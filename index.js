145c14b19977662cf82d0cea3e297a613df8fda2
nekkyi <144165459+nekkyi@users.noreply.github.com>
2024-05-22 10:25:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function criarTabela(){
+const table = document.createElement("table")
+const thead = document.createElement("thead")
+const tbody = document.createElement("tbody")
+
+const tr_nome = document.createElement("tr")
+const td_nome = document.createElement("td")
+td_nome.innerHTML = nome;
+td_nome.coldSpan = 5;
+tr_nome.appendChild(td_nome);
+
+const header = ['b', 'i', 'n', 'g', 'o'];
+
+header.forEach(elemento => {
+const th document.createElement
+^("th");
+th.innerHTML = elemento;
+thead.appendChild(th);
+
+});
+ for (let i = 0 <5; i++){
+const tr =document.createelement
+("tr");
+for (let j = 0;)
+
+ }
+
+
+}
