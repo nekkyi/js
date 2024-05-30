bc1be334319fecf0e97c361c3c315e3140e1b538
nekkyi <144165459+nekkyi@users.noreply.github.com>
2024-05-29 23:01:17 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-function criarTabela(){
-const table = document.createElement("table")
-const thead = document.createElement("thead")
-const tbody = document.createElement("tbody")
+var jogadores = []
 
-const tr_nome = document.createElement("tr")
-const td_nome = document.createElement("td")
-td_nome.innerHTML = nome;
-td_nome.coldSpan = 5;
-tr_nome.appendChild(td_nome);
+function gerarNumerosAleatorios(quantidade, min, max){
 
-const header = ['b', 'i', 'n', 'g', 'o'];
+    if(quantidade > (max - min)){
+        console.log("Intervalo insuficiente ...");
+        return;
+    }
 
-header.forEach(elemento => {
-const th document.createElement
-^("th");
-th.innerHTML = elemento;
-thead.appendChild(th);
+    var numeros = [];
 
-});
- for (let i = 0 <5; i++){
-const tr =document.createelement
-("tr");
-for (let j = 0;)
+    while(numeros.length < quantidade){
+        var aleatorio = Math.floor(Math.random()*(max - min) + min);
+        
+        if(!numeros.includes(aleatorio)){
+            numeros.push(aleatorio);
+        }
+    }
 
- }
+    return numeros;
 
+}
 
+function gerarCartela(){
+    var nomeJogador = prompt('Digite o nome do jogador');
+
+    var cartela = [gerarNumerosAleatorios(5,1,15), gerarNumerosAleatorios(5,16,30), gerarNumerosAleatorios(5,31,45),gerarNumerosAleatorios(5,46,60), gerarNumerosAleatorios(5,61,75)]
+
+    jogadores.push({
+        nomeJogador: nomeJogador,
+        cartela: cartela
+    });
+
+    desenharCartela(nomeJogador, cartela);
+
+    console.log(jogadores)
+}
+
+function reiniciarJogo(){
+    jogadores = []
+}
+
+function desenharCartela(nome, cartela){
+    var div = document.getElementById('espaco_cartelas');
+
+    var tabela = document.createElement('table');
+
+    var thead = document.createElement('thead');
+
+    var thB = document.createElement('th');
+    thB.innerText = 'B';
+    var thI = document.createElement('th');
+    thI.innerText = 'I';
+    var thN = document.createElement('th');
+    thN.innerText = 'N';
+    var thG = document.createElement('th');
+    thG.innerText = 'G';
+    var thO = document.createElement('th');
+    thO.innerText = 'O';
+
+    thead.appendChild(thB)
+    thead.appendChild(thI)
+    thead.appendChild(thN)
+    thead.appendChild(thG)
+    thead.appendChild(thO)
+
+    for(var i = 0; i < 5; i++){
+        var tr = document.createElement('tr');
+        for(var j = 0; j < 5; j++){
+            var td = document.createElement('td');
+            if(i == 2 && j == 2){
+                td.innerText = "X";
+                tr.appendChild(td);
+            }else{
+                td.innerText = cartela[j][i]
+                tr.appendChild(td);
+            }
+        }
+        tabela.appendChild(tr)
+    }
+
+    tabela.appendChild(thead);
+    div.appendChild(tabela);
 }
